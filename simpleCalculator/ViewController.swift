4399d6ecb0c2bc7c6053f72e5ce9809bb6a3af9b
raynelz <97597336+zaharichswiftly@users.noreply.github.com>
2023-06-04 12:32:37 +0300

    Initial Commit

---
+++ simpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simpleCalculator
+//
+//  Created by Захар Литвинчук on 04.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
